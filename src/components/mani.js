4735982e5dcf1028a6b736ac4013a88b44562f7c
Manikanta <mani517.m5@gmail.com>
2023-10-19 12:25:02 +0530

    second

---
+++ src/components/mani.js
@@ -0,0 +1,11 @@
+import React from"react";
+import{AiFillMail} from "react-icons/ai";
+
+function Mani(){
+        return(
+    <div>
+<p>Manikanta</p><AiFillMail className="mail"/>
+    </div>
+        )
+}
+export default Mani
